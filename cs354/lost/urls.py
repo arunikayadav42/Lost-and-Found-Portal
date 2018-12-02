bab0ca85ebda2ec9c0b3a17bb77f47b81303798d
arunikaydav42 <arunikayadav42@gmail.com>
2018-12-02 12:52:20 +0530

    Original changes made for the comment section removed

--- cs354/lost/urls.py
+++ cs354/lost/urls.py
@@ -6,8 +6,6 @@ from . import views
 urlpatterns = [
     path('<int:pk>/delete/', LostDeleteView.as_view(), name="lost_delete"),
     path('<int:pk>/edit/', LostUpdateView.as_view(), name="lost_edit"),
-    path('<int:pk>/comment/', views.add_comment_to_lost, 
-         name="add_comment_to_lost"),
     path('create/', LostCreateView.as_view(), name="lost_create"),
     path('<int:pk>/', LostDetailView.as_view(), name="lost_detail"),
     path('', LostListView.as_view(), name="lost_list"),
