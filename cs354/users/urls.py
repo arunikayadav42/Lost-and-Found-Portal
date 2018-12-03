eaf842cda874392e039c8977186a1655144041db
mayank <wadhwanimayank39@gmail.com>
2018-12-03 13:31:20 +0530

    Check profile now using roll number slug

--- cs354/users/urls.py
+++ cs354/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SignUpView, UserProfile
+from .views import SignUpView, UserProfile, UserDetailView
 from . import views
 
 urlpatterns = [
 	path('signup/', SignUpView.as_view(), name='signup'),
 	path('login/profile/', UserProfile.as_view(), name='userprofile'),
 	path('login/profile/edit/', views.edit_profile, name='editprofile'),
-]
+	path('<slug:slug>/', UserDetailView.as_view(), name="profile_detail"),
+]	
