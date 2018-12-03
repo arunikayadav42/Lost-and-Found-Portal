b7473e2993c03467645a83e5931d1f7471a41bf1
mayank <wadhwanimayank39@gmail.com>
2018-12-03 20:05:19 +0530

    Profile View Updated

--- cs354/users/urls.py
+++ cs354/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SignUpView, UserProfile
+from .views import SignUpView, UserProfile, ProfileUpdateView, UserDetailView
 from . import views
 
 urlpatterns = [
 	path('signup/', SignUpView.as_view(), name='signup'),
 	path('login/profile/', UserProfile.as_view(), name='userprofile'),
-	path('login/profile/edit/', views.edit_profile, name='editprofile'),
-	path('profile/<slug:slug>/', views.user_detail, name="profile_detail"),
+	path('login/profile/edit/', ProfileUpdateView.as_view(), name='editprofile'),
+	path('profile/<slug:slug>/', UserDetailView.as_view(), name="profile_detail"),
 ]	
--- cs354/users/views.py
+++ cs354/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, FormView, DetailView
+from django.views.generic import CreateView, FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 
 class SignUpView(CreateView):
@@ -17,20 +19,18 @@ class UserProfile(FormView):
     form_class = CustomUserCreationForm
 
 
-def edit_profile(request):
-    if request.method == 'POST':
-        form = CustomUserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('userprofile')
-
-    else:
-        form = CustomUserChangeForm(instance=request.user)
-        args = {'form': form}
-        return render(request, 'users/editprofile.html', args)
-    return redirect('userprofile')
+class UserDetailView(DetailView):
+    model = CustomUser
+    template_name = "users/profileview.html"
 
 
-def user_detail(request, **kwargs):
-    customuser = get_object_or_404(CustomUser, slug=kwargs.get('slug'))
-    return render(request, 'users/profileview.html', {'customuser': customuser})
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    template_name = 'users/editprofile.html'
+    success_url = reverse_lazy('userprofile')
+
+    def get_object(self):
+        return CustomUser.objects.get(username=self.request.user)
+
+
